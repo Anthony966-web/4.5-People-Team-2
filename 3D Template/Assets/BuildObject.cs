a6db04ff6cd090464fa21f5924e63ebc29dbf742
1722415 <1722415@mukilteo.wednet.edu>
2025-03-26 10:15:11 -0700

    no message

---
+++ 3D Template/Assets/BuildObject.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BuildObject : MonoBehaviour
+{
+
+    public bool foundation;
+    public List<Collider> col = new List<Collider>();
+    public Material Green;
+    public Material Red;
+    public bool IsBuildable;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8 && foundation)
+        {
+            col.Add(other);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 8 && foundation)
+        {
+            col.Remove(other);
+        }
+    }
+
+    private void Update()
+    {
+        changeColor();
+    }
+
+    public void changeColor()
+    {
+        if (col.Count == 0)
+            IsBuildable = true;
+        else
+            IsBuildable = false;
+        if(IsBuildable)
+        {
+            foreach(Transform child in this.transform)
+            {
+                child.GetComponent<Renderer>().material = Green;
+            }
+        }
+        else
+        {
+            foreach (Transform child in this.transform)
+            {
+                child.GetComponent<Renderer>().material = Red;
+            }
+        }
+    }
+}
