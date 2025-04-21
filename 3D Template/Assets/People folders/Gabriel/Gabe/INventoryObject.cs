0560a27ea886c46b846cd719955636429ee4a31f
MUKILTEO\1721488 <Gabrielrocha2007brasil@gmail.com>
2025-04-21 10:15:15 -0700

    hello

--- 3D Template/Assets/People folders/Gabriel/Gabe/INventoryObject.cs
+++ 3D Template/Assets/People folders/Gabriel/Gabe/INventoryObject.cs
@@ -1,15 +1,29 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class INventoryObject : MonoBehaviour
 {
     public ItemAssets inventoryItem;
+    private float distanced;
+    private float distancebetweentarget = 3;
+    public GameObject player;
 
-    private void OnCollisionEnter(Collision collision)
+    public void Start()
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            FindFirstObjectByType<Inventory>().AddItem(inventoryItem.inventoryItem, this.gameObject);
-            //Destroy(this.gameObject);
-        }
+        player = GameObject.FindGameObjectWithTag("Player");
     }
+    public void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, distancebetweentarget) && Input.GetKeyDown(KeyCode.H))
+        {
+            if (hit.collider.gameObject == this.gameObject)
+            {
+                FindFirstObjectByType<Inventory>().AddItem(inventoryItem.inventoryItem, this.gameObject);
+            }
+        }
+        //Destroy(this.gameObject);
+    }    
+    
 }
---
+++ 3D Template/Assets/People folders/Gabriel/Gabe/testing/AttributesManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class AttributesManager : MonoBehaviour
+{
+    public int health;
+    public int attack;
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+    }
+
+    public void DealDamage(GameObject target)
+    {
+        var atm = target.GetComponent<AttributesManager>();
+        if (atm != null )
+        {
+            atm.TakeDamage(attack);
+        }
+    }
+
+}
