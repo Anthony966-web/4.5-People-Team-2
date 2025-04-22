4c19a88d3cb7170b39ba75ef1848721e27b6c864
DESKTOP-U2PUTUT\User <1722415@mukilteo.wednet.edu>
2025-04-22 06:14:35 -0700

    no message

--- 3D Template/Assets/People folders/Gabriel/Gabe/INventoryObject.cs
+++ 3D Template/Assets/People folders/Gabriel/Gabe/INventoryObject.cs
@@ -1,18 +1,21 @@
-using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class INventoryObject : MonoBehaviour
 {
     public ItemAssets inventoryItem;
     //private float distanced;
-    private float distancebetweentarget = 20;
+    private float distancebetweentarget = 3;
     public GameObject player;
+
+    public GameObject textContainer;
 
 
 
     public void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        textContainer = GameObject.Find("Canvas").transform.Find("CrossHair").transform.Find("ItemFile").gameObject;
     }
     public void Update()
     {
@@ -21,6 +24,9 @@ public class INventoryObject : MonoBehaviour
         if (Physics.Raycast(ray, out RaycastHit hit, distancebetweentarget))
         {
             Debug.Log("Pick Up UI");
+            textContainer.SetActive(true);
+            textContainer.GetComponent<TMP_Text>().text = inventoryItem.name + " x" + inventoryItem.inventoryItem.quantity + " [B]";
+
             if (hit.collider.gameObject == this.gameObject && Input.GetKeyDown(KeyCode.B))
             {
                 //FindFirstObjectByType<Inventory>().AddItem(inventoryItem.inventoryItem, this.gameObject);
@@ -39,6 +45,7 @@ public class INventoryObject : MonoBehaviour
         else
         {
             Debug.Log("No Pick Up UI");
+            textContainer.SetActive(false);
         }
     }
 
