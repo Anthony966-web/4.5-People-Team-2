49d35e225016a4eed2a8c757f7285695cd8f8990
MUKILTEO\1720941 <jtkid2@icloud.com>
2025-04-22 10:14:10 -0700

    Penitratimng-cum-blast (indian tank round(120mm))

---
+++ 3D Template/Assets/Scripts/random tree spawner.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class randomtreespawner : MonoBehaviour
+{
+    [Header ("Spawn Settings")]
+    public GameObject resourcePrefab;
+    public float spawnChance;
+
+    [Header("Raycast Settings")]
+    public float distanceBetweenCheck;
+    public float heightOfCheck = 10f, rangeOfCheck = 30f;
+    public LayerMask layerMask;
+    public Vector2 positivePosition, negativePosition;
+
+
+    private void Start()
+    {
+        SpawnResources();
+    }
+
+    void SpawnResources()
+    {
+        for(float x = negativePosition.x; x < positivePosition.x; x += distanceBetweenCheck)
+        {
+            for (float z = negativePosition.y; z < positivePosition.y; z += distanceBetweenCheck)
+            {
+                RaycastHit hit;
+                if(Physics.Raycast(new Vector3(x, heightOfCheck, z), Vector3.down, out hit, rangeOfCheck, layerMask))
+                {
+                    Instantiate(resourcePrefab, hit.point, Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0)), transform);
+                }
+            }
+        }
+    }
+}
