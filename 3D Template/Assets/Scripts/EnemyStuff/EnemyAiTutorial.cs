30d5fcd4a86953b28f34a6406a2775e224465ba2
MUKILTEO\1721488 <Gabrielrocha2007brasil@gmail.com>
2025-05-08 08:37:38 -0700

    a

--- 3D Template/Assets/Scripts/EnemyStuff/EnemyAiTutorial.cs
+++ 3D Template/Assets/Scripts/EnemyStuff/EnemyAiTutorial.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.AI;
 using System.Collections;
 using Unity.VisualScripting;
-using UnityEngine.UIElements;
 
 public class EnemyAiTutorial : MonoBehaviour
 {
@@ -13,13 +12,13 @@ public class EnemyAiTutorial : MonoBehaviour
     public LayerMask WhatIsGround, WhatIsPlayer;
     public GameObject Projectile;
     public float health;
+    public Animator animator;
+    public bool IsRunning;
 
     //patrol
     public Vector3 walkpoint;
     bool walkPointset;
     public float walkpointRange;
-    public float FullsSpeed;
-    public float walkSpeed;
 
     //attacking
     public float timeBetweenAttacks;
@@ -31,6 +30,7 @@ public class EnemyAiTutorial : MonoBehaviour
 
     private void Update()
     {
+        animator.SetBool("IsRunning", IsRunning);
         ////check for sight and attack range
         PlayerInSightRange = Physics.CheckSphere(transform.position, sightRange, WhatIsPlayer);
         PlayerInAttackRange = Physics.CheckSphere(transform.position, attackRange, WhatIsPlayer);
@@ -56,6 +56,8 @@ public class EnemyAiTutorial : MonoBehaviour
 
     private IEnumerator Patroling()
     {
+        IsRunning = false;
+        Agent.isStopped = false;
         if (!walkPointset)
         { SearchWalkPoint(); }
         if (walkPointset)
@@ -64,29 +66,38 @@ public class EnemyAiTutorial : MonoBehaviour
 
             Vector3 distanceToWalkPoint = transform.position - walkpoint;
 
-            if (distanceToWalkPoint.magnitude < 2f)
+            if (distanceToWalkPoint.magnitude < 1f)
             {
-                yield return new WaitForSeconds(3);
+                yield return new WaitForSeconds(1);
+                walkPointset = false;
+            }
+            if (distanceToWalkPoint.magnitude > 1f)
+            {
+                yield return new WaitForSeconds(10);
                 walkPointset = false;
             }
         }
     }
     private void ChasePlayer()
     {
-<<<<<<< HEAD
         IsRunning = true;
         Agent.isStopped = false;
-        Agent.speed = FullsSpeed;
-=======
         Agent.speed = 10;
->>>>>>> 3d8dcbb4693937b828993bb20f417b023b5e7dcc
         Agent.SetDestination(Player.position);
     }
 
     private void AttackPlayer()
     {
+        IsRunning = false;
+        if (Agent.isActiveAndEnabled)
+        {
+            Agent.isStopped = true;
+        }
         Agent.SetDestination(transform.position);
-        transform.LookAt(Player);
+        Vector3 lookPosition = Player.position;
+        lookPosition.y = transform.position.y; // Keep the AI's y-axis unchanged
+        transform.LookAt(lookPosition);
+        //transform.forward = GetComponent<Rigidbody>().linearVelocity;
 
         if (!alreadyAttacked)
         {
@@ -129,9 +140,9 @@ public class EnemyAiTutorial : MonoBehaviour
         float randomX = Random.Range(-walkpointRange, walkpointRange);
         
         walkpoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-        if (Physics.Raycast(walkpoint, -transform.up, 5f, WhatIsGround))
+        if (Physics.Raycast(walkpoint, -transform.up, 2f, WhatIsGround))
         {
-            Agent.speed = walkSpeed;
+            Agent.speed = 3.5f;
             walkPointset = true;
 
         }
